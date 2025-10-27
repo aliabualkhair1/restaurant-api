fcbfc15326d85cc5d70811967a5bdf43fa699dec
aliabualkhair1 <156579098+aliabualkhair1@users.noreply.github.com>
2025-10-27 23:12:08 +0300

    Delete DTOs directory

--- DTOs/Models/Update/UpdateOrderItemsDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using DAL.Entities.Enums;
-namespace DAL.DTOs.Models.Update
-{
-    public class UpdateOrderItemsDTO
-    {
-        public int? MenuItemId { get; set; }
-        public int? Quantity { get; set; }
-    }
-}
